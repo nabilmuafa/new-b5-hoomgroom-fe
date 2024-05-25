fc5493de9b8e067c6a6f2d28a93b33f1c7cd9382
Fernando Valentino Sitinjak <112821721+Scarletra@users.noreply.github.com>
2024-05-26 01:23:55 +0700

    Implement filter by search frontend and create detail product dir

--- app/search/[keyword]/page.jsx
+++ app/search/[keyword]/page.jsx
@@ -1,7 +1,3 @@
-import Link from "next/link";
-import AnimeList from "@/components/AnimeList";
-import Header from "@/components/AnimeList/Header";
-
 import ProductList from "../../components/product-card";
 import { getSearchedProduct } from "../../../libs/api-libs";
 
