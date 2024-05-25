142a05e3d28532006a11ee2fa25c2f665d05f8fd
Fernando Valentino Sitinjak <112821721+Scarletra@users.noreply.github.com>
2024-05-25 20:11:59 +0700

    Unknown commit

--- app/components/product-card.jsx
+++ app/components/product-card.jsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 
 const Card = () => {
     return (
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mx-2 my-2">
+        <div className="grid grid-cols-3 lg:grid-cols-5 gap-4 mx-2 my-2">
             <div className="max-w-xs rounded overflow-hidden shadow-lg">
                 <Link href={"google.com"}>
                     <Image className="w-full h-auto" src="next.svg" width={200} height={150} alt="Placeholder Image" />
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">Dummy Title</div>
-                        <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="text-gray-700 text-base max-h-40 overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center mx-4 p-2">
+                        <p className="font-bold text-l">Stock: 10</p>
+                        <p className="text-red-500 font-bold text-l">Rp 20.000</p>
                     </div>
                 </Link>
             </div>
@@ -19,7 +23,11 @@ const Card = () => {
                     <Image className="w-full h-auto" src="next.svg" width={200} height={150} alt="Placeholder Image" />
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">Dummy Title</div>
-                        <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="text-gray-700 text-base max-h-40 overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center mx-4 p-2">
+                        <p className="font-bold text-l">Stock: 10</p>
+                        <p className="text-red-500 font-bold text-l">Rp 20.000</p>
                     </div>
                 </Link>
             </div>
@@ -29,7 +37,11 @@ const Card = () => {
                     <Image className="w-full h-auto" src="next.svg" width={200} height={150} alt="Placeholder Image" />
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">Dummy Title</div>
-                        <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="text-gray-700 text-base max-h-40 overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center mx-4 p-2">
+                        <p className="font-bold text-l">Stock: 10</p>
+                        <p className="text-red-500 font-bold text-l">Rp 20.000</p>
                     </div>
                 </Link>
             </div>
@@ -39,7 +51,39 @@ const Card = () => {
                     <Image className="w-full h-auto" src="next.svg" width={200} height={150} alt="Placeholder Image" />
                     <div className="px-6 py-4">
                         <div className="font-bold text-xl mb-2">Dummy Title</div>
-                        <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="text-gray-700 text-base max-h-40 overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center mx-4 p-2">
+                        <p className="font-bold text-l">Stock: 10</p>
+                        <p className="text-red-500 font-bold text-l">Rp 20.000</p>
+                    </div>
+                </Link>
+            </div>
+
+            <div className="max-w-xs rounded overflow-hidden shadow-lg">
+                <Link href={"google.com"}>
+                    <Image className="w-full h-auto" src="next.svg" width={200} height={150} alt="Placeholder Image" />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Dummy Title</div>
+                        <p className="text-gray-700 text-base max-h-40 overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center mx-4 p-2">
+                        <p className="font-bold text-l">Stock: 10</p>
+                        <p className="text-red-500 font-bold text-l">Rp 20.000</p>
+                    </div>
+                </Link>
+            </div>
+
+            <div className="max-w-xs rounded overflow-hidden shadow-lg">
+                <Link href={"google.com"}>
+                    <Image className="w-full h-auto" src="next.svg" width={200} height={150} alt="Placeholder Image" />
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">Dummy Title</div>
+                        <p className="text-gray-700 text-base max-h-40 overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center mx-4 p-2">
+                        <p className="font-bold text-l">Stock: 10</p>
+                        <p className="text-red-500 font-bold text-l">Rp 20.000</p>
                     </div>
                 </Link>
             </div>
