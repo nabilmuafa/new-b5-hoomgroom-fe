c5b80cc351ae5649a865f2066e0b69287b553c27
Fernando Valentino Sitinjak <112821721+Scarletra@users.noreply.github.com>
2024-05-26 20:16:43 +0700

    change localhost into deployment

--- libs/api-libs.js
+++ libs/api-libs.js
@@ -1,13 +1,13 @@
 export const getAllProducts = async () => {
-    const response =  await fetch('http://localhost:8080/admin/product/list', { next: { cache: 'no-store', revalidate: 0 } })
+    const response =  await fetch('http://35.197.129.191/admin/product/list', { next: { cache: 'no-store', revalidate: 0 } })
     const products = await response.json()
     return products
 }
 
 export const getFilteredProduct = async (filterType, amount, fromLowest, keyword) => {
     const url = keyword ?
-        `http://localhost:8080/admin/product/filter?filterType=${filterType}&amount=${amount}&fromLowest=${fromLowest}&keyword=${keyword}` :
-        `http://localhost:8080/admin/product/filter?filterType=${filterType}&amount=${amount}&fromLowest=${fromLowest}`;
+        `http://35.197.129.191/admin/product/filter?filterType=${filterType}&amount=${amount}&fromLowest=${fromLowest}&keyword=${keyword}` :
+        `http://35.197.129.191/admin/product/filter?filterType=${filterType}&amount=${amount}&fromLowest=${fromLowest}`;
 
     const response = await fetch(url);
     const products = await response.json();
