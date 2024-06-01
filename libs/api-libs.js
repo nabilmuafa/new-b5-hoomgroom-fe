0c40c8dd24b6349e2989d44f72eb736f989bba0c
nabilmuafa <nmuafa@gmail.com>
2024-06-01 17:44:38 +0700

    change api route using https

--- libs/api-libs.js
+++ libs/api-libs.js
@@ -1,21 +1,30 @@
 export const getAllProducts = async () => {
-    const response =  await fetch('http://35.197.129.191/admin/product/list', { next: { cache: 'no-store', revalidate: 0 } })
-    const products = await response.json()
-    return products
-}
+  const response = await fetch("https://api.b5-hoomgroom/admin/product/list", {
+    next: { cache: "no-store", revalidate: 0 },
+  });
+  const products = await response.json();
+  return products;
+};
 
-export const getFilteredProduct = async (filterType, amount, fromLowest, keyword) => {
-    const url = keyword ?
-        `https://api.b5-hoomgroom.com/admin/product/filter?filterType=${filterType}&amount=${amount}&fromLowest=${fromLowest}&keyword=${keyword}` :
-        `https://api.b5-hoomgroom.com/admin/product/filter?filterType=${filterType}&amount=${amount}&fromLowest=${fromLowest}`;
+export const getFilteredProduct = async (
+  filterType,
+  amount,
+  fromLowest,
+  keyword
+) => {
+  const url = keyword
+    ? `https://api.b5-hoomgroom.com/admin/product/filter?filterType=${filterType}&amount=${amount}&fromLowest=${fromLowest}&keyword=${keyword}`
+    : `https://api.b5-hoomgroom.com/admin/product/filter?filterType=${filterType}&amount=${amount}&fromLowest=${fromLowest}`;
 
-    const response = await fetch(url);
-    const products = await response.json();
-    return products;
-}
+  const response = await fetch(url);
+  const products = await response.json();
+  return products;
+};
 
 export const getDetailProduct = async (productId) => {
-    const response = await fetch(`https://api.b5-hoomgroom.com/admin/product/update/${productId}`)
-    const product = await response.json()
-    return product
-}
+  const response = await fetch(
+    `https://api.b5-hoomgroom.com/admin/product/update/${productId}`
+  );
+  const product = await response.json();
+  return product;
+};
